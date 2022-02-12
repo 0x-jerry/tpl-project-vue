9c301aae2d26ca6f5c5b07727f8cabe7cac5889f
Jerry Wang <x.jerry.wang@gmail.com>
2022-02-12 22:38:14 +0800

    feat: add unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import components from 'unplugin-vue-components/vite'
 import layouts from 'vite-plugin-vue-layouts'
 import pages from 'vite-plugin-pages'
 import windicss from 'vite-plugin-windicss'
+import icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -33,6 +35,9 @@ export default defineConfig(({ mode }) => {
     plugins: [
       vue(),
 
+      // https://github.com/antfu/unplugin-icons
+      icons(),
+
       // https://github.com/antfu/unplugin-auto-import
       imports({
         dts: 'src/auto-imports.d.ts',
@@ -41,8 +46,9 @@ export default defineConfig(({ mode }) => {
 
       // https://github.com/antfu/unplugin-vue-components
       components({
-        dts: 'src/components.d.ts',
+        dts: 'src/auto-components.d.ts',
         dirs: ['src/components', 'src/biz-components'],
+        resolvers: [IconsResolver()],
       }),
 
       // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
