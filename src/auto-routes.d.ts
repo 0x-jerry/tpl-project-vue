d6095f24f544b947e0340a7c733cf6076205b6f3
Jerry <x.jerry.wang@gmail.com>
2023-08-26 21:05:23 +0800

    fix: redirect routes

--- src/auto-routes.d.ts
+++ src/auto-routes.d.ts
@@ -39,9 +39,8 @@ import type {
 
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
-  export interface RouteNamedMap {
-    '//': RouteRecordInfo<'//', '/', Record<never, never>, Record<never, never>>,
-  }}
+    '/home/': RouteRecordInfo<'/home/', '/home', Record<never, never>, Record<never, never>>,
+  }
 }
 
 declare module 'vue-router/auto' {
--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,14 +1,21 @@
 import { setupLayouts } from 'virtual:generated-layouts'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router'
 import { routes as generatedRoutes } from 'vue-router/auto/routes'
 import { Plugin } from 'vue'
+
+const staticRoutes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/home',
+  },
+]
 
 export const install: Plugin = (app) => {
   const routes = setupLayouts(generatedRoutes)
 
   const router = createRouter({
     history: createWebHashHistory(),
-    routes,
+    routes: [...staticRoutes, ...routes],
   })
 
   app.use(router)
